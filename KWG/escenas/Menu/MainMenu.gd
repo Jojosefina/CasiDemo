fdf3874746868da9fd48518153aa8edd5ef86149
Jojosefina <josefina.bustos.g@ug.uchile.cl>
2022-06-20 18:29:08 -0400

    Creacion menu principal con Jugar y Salir

---
+++ KWG/escenas/Menu/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+onready var opt_menu = $OptionsMenu
+onready var video_menu = $OptionsMenu/VBoxContainer/SetBtn/Video
+
+
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Game.tscn")
+
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+	
